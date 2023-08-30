576446e6428549c7c3799374357652c9dcaa600b
seungchae <s2125@e-mirim.hs.kr>
2023-08-30 12:14:38 +0900

    딜리게이터와 람다를 이용해서 다양한 출력 구현

--- DelegatorStudent/Program.cs
+++ DelegatorStudent/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DelegatorStudent31;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,29 @@ namespace DelegatorStudent
     {
         static void Main(string[] args)
         {
+            Students students = new Students();
+            students.Add(new Student("함기훈", 4.5));
+            students.Add(new Student("박성래", 4.4));
+            students.Add(new Student("마이콜", 1.5));
+
+            students.Print();
+
+            students.Print((student) =>
+            {
+                Console.WriteLine("***********");
+                Console.WriteLine(" 이름: " + student.Name);
+                Console.WriteLine(" 학점: " + student.Score);
+            });
+
+            Console.WriteLine("<students>");
+            students.Print((s) =>
+            {
+                Console.WriteLine("  <student>");
+                Console.WriteLine("    <name>" + s.Name + "</name>");
+                Console.WriteLine("    <score>" + s.Score + "</score>");
+                Console.WriteLine("  </student>");
+            });
+            Console.WriteLine("/<students>");
         }
     }
 }
