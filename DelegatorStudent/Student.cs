c748923077731b8e5b0c088096ddb048cc7c5ee2
seungchae <s2125@e-mirim.hs.kr>
2023-08-30 11:56:16 +0900

    Student 클래스 생성

---
+++ DelegatorStudent/Student.cs
@@ -0,0 +1,17 @@
+﻿namespace DelegatorStudent
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public double Score { get; set; }
+        public Student(string name, double score)
+        {
+            Name = name;
+            Score = score;
+        }
+        public override string ToString()
+        {
+            return Name + " : " + Score;
+        }
+    }
+}
